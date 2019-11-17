4a0236814451a82d0290c04df318af43a9367dfe
AMINUL-IT-SOLUTIONS <AMINUL-IT-SOLUTIONS@DESKTOP-E2NQQN2>
2019-11-17 10:05:32 -0800

    5th commit

---
+++ src/odd/even/ODDEVEN.java
@@ -0,0 +1,20 @@
+
+package odd.even;
+
+
+public class ODDEVEN {
+
+    
+    public static void main(String[] args) {
+        for( int a=1; a<10; a+=2){
+            System.out.println(a);
+           
+    }
+        for(int a=1;a<10; a++){
+            if(a%2==0){
+                System.out.println(a);
+            }
+        }
+    
+}
+}
